63b29e1fea88103ef77db3973097708eaf64e250
Py Supply <pysupply@M1-MacAir.local>
2023-06-08 17:38:03 -0400

    variables in python

---
+++ variables.py
@@ -0,0 +1,11 @@
+#integers - whole numbers, 7, 10,15
+age = 25
+print(age)
+# floats - decimal numbers, 7.1, 8.2, 3.14156
+height = 72.5
+print(height)
+# strings - hello world
+name = 'py supply'
+print(name)
+
+print('your age is ' +  str(age) + ' your height is ' + str(height) + ' your name is ' + str(name))
